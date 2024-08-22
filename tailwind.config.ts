3ea01feb51916a25014943c0b5a7ea7cd37b8df8
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-22 21:53:15 +0300

    [Build]: Basic configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          yellow: "#FCBF01",
+          blue: "#3C45B9",
+          black: "#000001",
+          milkBrown: "#EFE3D4",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
