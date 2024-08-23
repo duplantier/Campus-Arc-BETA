217ce5ab7be2a38cc6bf207da3df676544c4f1ad
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-23 13:25:04 +0300

    [Build]: Home & About Tab

--- components/home/AboutTab.tsx
+++ components/home/AboutTab.tsx
@@ -1,16 +1,148 @@
 import React from "react";
+import { CardSpotlight } from "../ui/card-spotlight";
+import Image from "next/image";
 
 const AboutTab = () => {
   /* 
-  1. What is Campus Arc
-  2. The problem with current college life
-  3. The solution Campus Arc provides
-  4. The Sustainable Development Goal (SDGs) Campus Arc aligns with
-  5. How will Campus Arc address this goal?
+    1. What is Campus Arc
+    2. The problem with current college life
+    3. The solution Campus Arc provides
+    4. The Sustainable Development Goal (SDGs) Campus Arc aligns with
+    5. How will Campus Arc address this goal?
   */
+
   return (
-    <section className="w-full overflow-auto flex gap-10 h-[80vh] mb-10 rounded-2xl text-xl md:text-4xl border  bg-gradient-to-b from-gray-950 to-gray-800">
-      <h1 className="text-center w-full text-gray-50 mt-10 text-5xl">About</h1>
+    <section className="w-full  flex-col items-center px-20 overflow-auto flex gap-10 h-[80vh] mb-16 rounded-2xl text-xl md:text-4xl border  bg-gray-950 ">
+      <h1 className="text-center w-full text-gray-50 mt-10 text-5xl righteous-text">
+        About Us
+      </h1>
+      <p className="text-lg text-center w-full -mt-8 text-gray-500">
+        Let's meet and shape the future together.
+      </p>
+      <div className="w-full flex justify-center items-center gap-10 h-auto">
+        <CardSpotlight className=" py-8 w-[55%] text-gray-50 bg-gradient-t-r from-gray-900 to-gray-800 h-full border border-gray-600 hover:border-brand-blue">
+          <div className="flex  gap-10 items-center z-50 mb-6">
+            <Image
+              src="/buyutec.svg"
+              alt="What is Campus Arc?"
+              width={180}
+              height={200}
+              className="z-50"
+            />
+            <h1 className="text-3xl relative z-20 righteous-text">
+              What is Campus Arc?
+            </h1>
+          </div>
+
+          <p className="z-20 text-xl relative mt-4">
+            Campus Arc is a next-gen web-based e-learning application where
+            college students can match with their campus fellows, build
+            meaningful projects in their field of expertise together, and
+            increase their competency by gaining project-based experiences.
+            <br />
+            <br />
+            Campus Arc aims to spread project-based competency in the scope of
+            college campuses and make graduations more meaningful
+          </p>
+        </CardSpotlight>
+        <CardSpotlight className=" py-8 w-[45%] text-gray-50 bg-gradient-t-r from-gray-900 to-gray-800 h-full border border-gray-600 hover:border-brand-blue">
+          <div className="flex gap-10 items-center z-50 mb-6">
+            <Image
+              src="/problem-girl.svg"
+              alt="The problem with current college life"
+              width={200}
+              height={200}
+              className="z-50"
+            />
+            <h1 className="font-righteous text-3xl relative z-20 righteous-text flex">
+              The Problem With Today's College Life
+            </h1>
+          </div>
+
+          <p className="z-20 text-xl relative mt-4">
+            Throughout their college life, students have difficulties to
+            involved in concrete projects on their expertise field. This
+            situation turns into an incredible career and life anxiety for
+            students when they graduate. They most likely face an 'inexperienced
+            wall' when they look for a job.
+          </p>
+          <div className="relative w-full"></div>
+        </CardSpotlight>
+      </div>
+      <div className="w-full flex justify-center items-center gap-10 h-auto">
+        <CardSpotlight className=" py-8 w-[50%] text-gray-50 bg-gradient-t-r from-gray-900 to-gray-800 h-full border border-gray-600 hover:border-brand-blue">
+          <div className="flex gap-10  items-center z-50 mb-6">
+            <Image
+              src="/solution-computer.svg"
+              alt="The solution Campus Arc provides"
+              width={200}
+              height={200}
+              className="z-50"
+            />
+            <h1 className="text-3xl relative z-20 righteous-text">
+              The Solution Campus Arc Provides
+            </h1>
+          </div>
+
+          <p className="z-20 text-xl relative mt-4">
+            With the help of Campus Arc, college students will have a chance to
+            find and meet other students from similar expertise fields with them
+            and on the same campus, be involved in a project together, and gain
+            project-based experience in their fields while doing so. Campus Arc
+            will play a guiding role with its engaging interface and qualified
+            Arc Modules that are developed in the light of instruction science.
+          </p>
+        </CardSpotlight>
+        <CardSpotlight className=" py-8 w-[50%] text-gray-50 bg-gradient-t-r from-gray-900 to-gray-800 h-full border border-gray-600 hover:border-brand-blue">
+          <div className="flex gap-10  items-center z-50 mb-6">
+            <Image
+              src="/world.svg"
+              alt="The solution Campus Arc provides"
+              width={120}
+              height={200}
+              className="z-50"
+            />
+            <h1 className="text-3xl relative z-20 righteous-text">
+              The Sustainable Development Goal (SDGs) Campus Arc aligns with
+            </h1>
+          </div>
+
+          <p className="z-20 text-xl relative mt-4">
+            We aim to reach Target 4.4 of the 4th Goal (Quality Education),
+            which is “By 2030, substantially increase the number of youth and
+            adults who have relevant skills, including technical and vocational
+            skills, for employment, decent jobs and entrepreneurship.” Also,
+            specifically, we want to reach Target 4.4.1: “Proportion of youth
+            and adults with information and communications technology (ICT)
+            skills, by type of skill.”
+          </p>
+        </CardSpotlight>
+      </div>
+
+      <div className="w-full flex justify-center items-center gap-10 h-auto mb-12">
+        <CardSpotlight className=" py-8 w-[60%] text-gray-50 bg-gradient-t-r from-gray-900 to-gray-800 h-full border border-gray-600 hover:border-brand-blue">
+          <div className="flex justify-center items-center z-50 mb-6">
+            <Image
+              src="/tohum.svg"
+              alt="Tohum Foundation"
+              width={400}
+              height={200}
+              className="z-50"
+            />
+          </div>
+          <h1 className="text-3xl relative z-20 righteous-text">
+            How will Campus Arc address this goal?
+          </h1>
+
+          <p className="z-20 text-xl relative mt-4">
+            We believe there are insufficient opportunities for youth to reveal
+            their potential through gaining project-based experience in their
+            expertise field. By providing proper conditions and chances, we can
+            significantly increase the number of competent young people and
+            fresh graduates.
+          </p>
+        </CardSpotlight>
+      </div>
     </section>
   );
 };
--- components/home/HomeTab.tsx
+++ components/home/HomeTab.tsx
@@ -12,34 +12,33 @@ const World = dynamic(() => import("../ui/globe").then((m) => m.World), {
 
 const HomeTab = () => {
   return (
-    <BackgroundBeamsWithCollision className="w-full overflow-hidden flex gap-10 relative h-[100vh] mb-10 rounded-2xl text-xl md:text-4xl border  bg-gradient-t-b from-gray-950 to-gray-800  ">
-      <div className="w-[60%] p-3 text-gray-50 flex flex-col gap-4 h-full  justify-center">
+    <BackgroundBeamsWithCollision className="w-full overflow-hidden flex gap-10 relative h-[100vh] mb-10 rounded-2xl text-xl md:text-4xl border  bg-gradient-t-b from-gray-950 to-gray-900  ">
+      <div className="w-full p-3 text-gray-50 flex flex-col gap-6 h-full items-center justify-center">
         <h1 className="text-6xl font-sans tracking-tight">
           E-learning.{" "}
           <span className=" font-bold py-4 text-brand-yellow [text-shadow:0_0_rgba(0,0,0,0.1)]">
             Reinvented.
           </span>
         </h1>
-        <p className="text- font-normal text-gray-300">
-          Introducing the next-generation e-learning experience, designed to
-          revolutionize{" "}
-          <strong className="text-brand-blueLight">
-            how college students connect and collaborate
+        <p className="text-center text-3xl font-normal text-gray-300">
+          Introducing the next-generation e-learning experience, designed to{" "}
+          <strong className="text-brand-yellow">
+            revolutionize how students connect and collaborate
           </strong>
           , leveraging Open Campus.
         </p>
         <Link
-          className="mt-6 text-xl flex items-center gap-2 hover:text-brand-yellow justify-center rounded-xl text-center border-2 px-4 py-3 bg-brand-black w-[220px] border-brand-yellow"
+          className=" text-xl flex items-center gap-2 text-white justify-center rounded-xl text-center border-2 px-4 py-3 bg-brand-black w-[220px] border-brand-blue hover:border-brand-yellow"
           href="/"
         >
           <Atom /> Get Started
         </Link>
       </div>
-      <div className="w-[45%] flex justify-center items-center h-full  p-30">
+      {/*  <div className="w-[45%] flex justify-center items-center h-full  p-30">
         <div className="absolute w-[600px] top-auto h-[80vh] z-10 ">
-          {/* <World data={globeSampleArcs} globeConfig={globeConfig} /> */}
+          <World data={globeSampleArcs} globeConfig={globeConfig} />
         </div>
-      </div>
+      </div> */}
     </BackgroundBeamsWithCollision>
   );
 };
