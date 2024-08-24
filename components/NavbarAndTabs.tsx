20841e914cd956c00a624b8c5ecbf454bf67679a
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-24 12:16:17 +0300

    [Fix]: sessionStorage is not defined error

--- components/NavbarAndTabs.tsx
+++ components/NavbarAndTabs.tsx
@@ -2,15 +2,14 @@
 import React from "react";
 
 import dynamic from "next/dynamic";
-import { Tabs } from "./ui/tabs";
 
-/* const ClientOnlyTabs = dynamic(() => import("@/components/ui/tabs") as any, {
+const ClientOnlyTabs = dynamic(() => import("@/components/ui/tabs") as any, {
   ssr: false,
-}); */
+});
 const NavbarAndTabs = () => {
   return (
     <div className=" md:h-[90vh] [perspective:1000px] ">
-      <Tabs />
+      <ClientOnlyTabs />
     </div>
   );
 };
