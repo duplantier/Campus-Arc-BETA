8a71277d1a647dba175b82c94fbefa4c0e332e82
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-24 12:19:59 +0300

    [Fix]: Client Only Importing Error

--- components/NavbarAndTabs.tsx
+++ components/NavbarAndTabs.tsx
@@ -3,9 +3,12 @@ import React from "react";
 
 import dynamic from "next/dynamic";
 
-const ClientOnlyTabs = dynamic(() => import("@/components/ui/tabs") as any, {
-  ssr: false,
-});
+const ClientOnlyTabs = dynamic(
+  () => import("@/components/ui/tabs").then((mod) => mod.Tabs),
+  {
+    ssr: false,
+  }
+);
 const NavbarAndTabs = () => {
   return (
     <div className=" md:h-[90vh] [perspective:1000px] ">
