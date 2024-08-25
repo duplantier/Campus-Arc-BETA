87fc095c1457ceab0f4efe4f490306fea1a922b5
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-25 14:37:38 +0300

    [Build]: Student API endpoint

--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -9,22 +9,93 @@ export async function POST(req: Request) {
     const {
       reqType,
       userObj,
+      adminKey,
     }: {
       reqType: string;
       userObj: Student;
+      adminKey: string;
     } = body;
 
+    if (adminKey !== process.env.ADMIN_KEY) {
+      return NextResponse.json({
+        error: "Unauthorized",
+        isCreated: false,
+        status: 401,
+      });
+    }
+
     if (reqType === "create") {
-      const student = await db.student.create({
+      const isStudentAlreadyExist = await db.student.findUnique({
+        where: { id: userObj.id },
+      });
+      if (isStudentAlreadyExist) {
+        return NextResponse.json({
+          error: "Student already exists",
+          isCreated: false,
+          status: 400,
+        });
+      } else {
+        const createdStudent = await db.student.create({
+          data: {
+            eduUsername: userObj.eduUsername || "",
+            email: userObj.email || "",
+            ethAddress: userObj.ethAddress || "",
+            OCaccessToken: userObj.OCaccessToken || "",
+            OCIdtoken: userObj.OCIdtoken || "",
+          },
+        });
+        return NextResponse.json({
+          createdStudent: createdStudent,
+          isCreated: true,
+          status: 201,
+        });
+      }
+    } else if (reqType === "update") {
+      const updatedStudent = await db.student.update({
+        where: { id: userObj.id }, // user id is required for updating
         data: {
           eduUsername: userObj.eduUsername || "",
           email: userObj.email || "",
           ethAddress: userObj.ethAddress || "",
           OCaccessToken: userObj.OCaccessToken || "",
           OCIdtoken: userObj.OCIdtoken || "",
+          arcModules: {
+            connect: userObj.arcModules?.map((module) => ({ id: module.id })),
+          },
+          registrationStakes: {
+            connect: userObj.registrationStakes?.map((stake) => ({
+              id: stake.id,
+            })),
+          },
         },
       });
-      return NextResponse.json({ student, isCreated: true, status: 201 });
+      return NextResponse.json({
+        updatedStudent: updatedStudent,
+        isUpdated: true,
+        status: 200,
+      });
+    } else if (reqType === "delete") {
+      const deletedStudent = await db.student.delete({
+        where: { id: userObj.id },
+      });
+      return NextResponse.json({
+        deletedStudent: deletedStudent,
+        isDeleted: true,
+        status: 200,
+      });
+    } else if (reqType === "read") {
+      const studentInfo = await db.student.findUnique({
+        where: { id: userObj.id },
+        include: {
+          arcModules: true,
+          registrationStakes: true,
+        },
+      });
+      return NextResponse.json({
+        studentInfo: studentInfo,
+        isRead: true,
+        status: 200,
+      });
     }
   } catch (error) {
     return NextResponse.json({ error: "Error", isCreated: false, status: 500 });
