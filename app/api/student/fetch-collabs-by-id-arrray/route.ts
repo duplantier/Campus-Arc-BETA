2a8a6cf86dcdb2e9874a03956dd10ace8530244a
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-27 19:20:40 +0300

    [Build]: Fetch Collab Students by Student ID Array API Endpoint

---
+++ app/api/student/fetch-collabs-by-id-arrray/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const {
+      adminKey,
+      studentIds,
+    }: {
+      adminKey: string;
+      studentIds: number[];
+    } = body;
+
+    if (adminKey !== process.env.NEXT_PUBLIC_ADMIN_KEY) {
+      return NextResponse.json({
+        error: "Unauthorized",
+        isCreated: false,
+        status: 401,
+      });
+    }
+
+    const collabStudents = await db.student.findMany({
+      where: {
+        id: {
+          in: studentIds,
+        },
+      },
+    });
+
+    return NextResponse.json({
+      collabStudents: collabStudents,
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error, isCreated: false, status: 500 });
+  }
+}
