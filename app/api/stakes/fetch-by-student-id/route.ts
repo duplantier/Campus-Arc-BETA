00f81fb509786ba53b3ce0c6e02b6055517547a2
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-27 19:20:19 +0300

    [Build]: Fetch Stake Record by Student id API Endpoint

---
+++ app/api/stakes/fetch-by-student-id/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const {
+      adminKey,
+      studentId,
+    }: {
+      adminKey: string;
+      studentId: number;
+    } = body;
+
+    if (adminKey !== process.env.NEXT_PUBLIC_ADMIN_KEY) {
+      return NextResponse.json({
+        error: "Unauthorized",
+        isCreated: false,
+        status: 401,
+      });
+    }
+
+    const studentsRegistrationStakes = await db.registrationStake.findMany({
+      where: {
+        studentId: studentId,
+      },
+    });
+
+    return NextResponse.json({
+      studentsRegistrationStakes: studentsRegistrationStakes,
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error, isCreated: false, status: 500 });
+  }
+}
