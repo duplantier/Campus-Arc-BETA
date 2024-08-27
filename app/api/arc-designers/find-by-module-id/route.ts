dd685cedad6227bf88ca4e1ccc79af2f7d147553
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-08-27 18:39:47 +0300

    [Build]: Fetch Arc Designer Info by Id API Endpoint

---
+++ app/api/arc-designers/find-by-module-id/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const {
+      adminKey,
+      arcModuleId,
+    }: {
+      adminKey: string;
+      arcModuleId: number;
+    } = body;
+
+    if (adminKey !== process.env.NEXT_PUBLIC_ADMIN_KEY) {
+      return NextResponse.json({
+        error: "Unauthorized",
+        isCreated: false,
+        status: 401,
+      });
+    }
+
+    const arcDesignersOfTheArcModule = await db.arcDesigner.findMany({
+      where: {
+        arcModulesIds: {
+          hasSome: [arcModuleId],
+        },
+      },
+    });
+
+    return NextResponse.json({
+      arcDesignersOfTheArcModule: arcDesignersOfTheArcModule,
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error, isCreated: false, status: 500 });
+  }
+}
